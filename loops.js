30a0f24a5e98bad878ad1484ee198e74af6ca00e
Jason Bamford <scarfandgoggles@yahoo.co.uk>
2019-02-22 23:22:46 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,22 @@
+
+
+
+function forLoop(a) {
+  for (var i = 0; i<25; ++i) {
+    a.push(`I am ${i} strange loop${ i<2 ? '' : 's' }.`)
+  }
+  return a;
+}
+
+function whileLoop(n) {
+  while (n--) {
+    console.log(n);
+  }
+  return 'done';
+}
+
+function doWhileLoop(n) {
+  do {
+    console.log("I run once regardless.");
+  } while(--n)
+}
